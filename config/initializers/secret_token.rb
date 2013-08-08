eef93fa3f820775f992b6d9c64398a5f9587d46a
Amy Erz <wingmerz@gmail.com>
2013-08-07 23:22:09 -0700

    install backbone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = 'ebadb1bcd8bf84a8cd739fd6578cbdc993d3a3b87fe00e798b3eec210cc7ba9620cd1aab2ff5d9d1101ba99d34c2f3aeb5fbfe7a2fc30dbe4e2accdb05141b4f'
